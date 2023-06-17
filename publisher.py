6a000a01144b0331c18c33cbb7faa8703ee5ce7c
Chris Mutchler <chris@virtualelephant.com>
2023-06-17 14:48:36 -0600

    added queue information

--- publisher.py
+++ publisher.py
@@ -2,19 +2,19 @@ import pika
 import time
 
 # Establish connection to RabbitMQ
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq', port=5672, credentials=pika.PlainCredentials('your-username', 'your-password')))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq', port=5672, credentials=pika.PlainCredentials('sample', 'VMware123!')))
 channel = connection.channel()
 
 # Declare a queue to publish messages
-channel.queue_declare(queue='your-queue-name')
+channel.queue_declare(queue='sample-application')
 
 # Publish messages to the queue
-messages = ["Message 1", "Message 2", "Message 3"]
+messages = ["INFO: Successfully initialized sample pplication version 0.1 ", "WARNING: Connection timeout issue while attempting to connect to sample application", "DEBUG: Executing background task: Cleaning up temp files"]
 
 for message in messages:
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
     body = f"{timestamp} - {message}"
-    channel.basic_publish(exchange='', routing_key='your-queue-name', body=body)
+    channel.basic_publish(exchange='', routing_key='sample-application', body=body)
     print(f"Published message: {body}")
 
 # Close the connection
