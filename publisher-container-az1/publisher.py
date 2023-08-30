44d63b296a9b0158a1fba955cc43d2007cfe3af3
Chris Mutchler <chris@virtualelephant.com>
2023-08-30 06:53:38 -0600

    Publisher updates for clustermesh
    Created an AZ1 and AZ2 version of Publisher container

---
+++ publisher-container-az1/publisher.py
@@ -0,0 +1,24 @@
+import pika
+import time
+
+host = 'rabbitmq.sample-app.svc.cluster.local'
+port = 5672
+credentials = pika.PlainCredentials('deploy', 'VMware123!', 'external')
+# Establish connection to RabbitMQ
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=host, port=port, credentials=credentials))
+channel = connection.channel()
+
+# Declare a queue to publish messages
+channel.queue_declare(queue='log-messages')
+
+# Publish messages to the queue
+messages = ["INFO: [AZ1] Successfully initialized Sample Application version 0.1 ", "INFO: [AZ1] Download Sample Application via Git from https://github.com/virtualelephant/k8s-sample-application.git", "INFO: [AZ1] Sample Application Log Message #1", "INFO: [AZ1] Sample Application Log Message #2", "INFO: [AZ1] Sample Application Log Message #3"]
+
+for message in messages:
+    timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+    body = f"{timestamp} - {message}"
+    channel.basic_publish(exchange='', routing_key='log-messages', body=body)
+    print(f"Published message: {body}")
+
+# Close the connection
+connection.close()
--- publisher-container-az2/publisher.py
+++ publisher-container-az2/publisher.py
@@ -12,7 +12,7 @@ channel = connection.channel()
 channel.queue_declare(queue='log-messages')
 
 # Publish messages to the queue
-messages = ["INFO: Successfully initialized sample application version 0.1 ", "WARNING: Connection timeout issue while attempting to connect to sample application", "DEBUG: Executing background task: Cleaning up temp files"]
+messages = ["INFO: [AZ2] Successfully initialized Sample Application version 0.1 ", "INFO: [AZ2] Download Sample Application via Git from https://github.com/virtualelephant/k8s-sample-application.git", "INFO: [AZ2] Sample Application Log Message #1", "INFO: [AZ2] Sample Application Log Message #2", "INFO: [AZ2] Sample Application Log Message #3"]
 
 for message in messages:
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
