cd9e24145f4b6f8add667b3525a5b2cccc08c4c5
Chris Mutchler <chris@virtualelephant.com>
2024-12-31 10:06:18 -0700

    PowerShell
    PowerShell files for creating VM environments

---
+++ PowerShell/create-env-opensource.ps1
@@ -0,0 +1,37 @@
+$vcServer = "vcenter-vcf01.home.virtualelephant.com"
+$vcUsername = "administrator@vsphere.local"
+$vcPassword = "thAswU53n#CE2025!!"
+
+Connect-VIServer -Server $vcServer -User $vcUsername -Password $vcPassword
+# Variables
+$templateName = "opensource-k8s-template"
+$destinationFolder = "OpenSource"
+$datastore = "Synology"  # Replace with your datastore
+$cluster = "ve-m01-cluster-001"      # Replace with your cluster
+
+$template = Get-VM -Name $templateName -ErrorAction Stop
+$cluster = Get-Cluster -Name $clusterName -ErrorAction Stop
+Write-Host "VM Template: $template"
+Write-Host "Cluster: $cluster"
+
+
+# Clone the template multiple times
+1..3 | ForEach-Object {
+    $vmName = "opensource-c1-cntrl-$($_)" # Define VM name dynamically
+    New-VM -Name $vmName -VM $template -Datastore $datastore -Location $destinationFolder -ResourcePool $cluster
+    Write-Host "Cloned VM: $vmName"
+}
+
+1..5 | ForEach-Object {
+    $vmName = "opensource-c1-node-$($_)" # Define VM name dynamically
+    New-VM -Name $vmName -VM $template -Datastore $datastore -Location $destinationFolder -ResourcePool $cluster
+    Write-Host "Cloned VM: $vmName"
+}
+
+# Create HAProxy Load Balancer
+$vmName = "opensource-c1-haproxy-1"
+New-VM -Name $vmName -VM $template -Datastore $datastore -Location $destinationFolder -ResourcePool $cluster
+Write-Host "Cloned VM: $vmName"
+
+# Disconnect from vCenter
+Disconnect-VIServer -Server $vcServer -Confirm:$false
